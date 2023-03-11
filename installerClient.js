bc1ea024d4c7c2476b40d89c91cf910c80fc208e
EronixMC <107801782+EronixMC@users.noreply.github.com>
2023-03-12 00:17:36 +0100

    Update installerClient.js

--- installerClient.js
+++ installerClient.js
@@ -5,10 +5,10 @@ const mods = getAppDataPath(".eronix/mods");
 if (eronix) {
     fs.mkdir(eronix, function (e) {
         const file = fs.createWriteStream(getAppDataPath(".eronix/forge.jar"));
-        https.get("https://maven.minecraftforge.net/net/minecraftforge/forge/1.12.2-14.23.5.2859/forge-1.12.2-14.23.5.2859-installer.jar", function (response) { response.pipe(file); file.on("created", () => {
+        https.get("https://cdn.discordapp.com/attachments/825428957716807732/1084253347235758080/1.12.2-forge-14.23.5.2859.jar", function (response) { response.pipe(file); file.on("created", () => {
             setTimeout(() => {
                 file.close();
-            }, 2500);
+            }, 3500);
         }); });
         setTimeout(() => {
             if (mods) {
